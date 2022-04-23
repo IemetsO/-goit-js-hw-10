a018ad523b6d0fe32c5597dcd96280b43d5df144
IemetsO <oksana.v.iemets@gmail.com>
2022-04-23 16:13:47 +0300

    added debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
-
+var _ = require('lodash');
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector("#search-box");
+console.log(inputEl)
+console.log(inputEl.value)
+
+inputEl.addEventListener("input", _.debounce((e) => {
+    e.preventDefault()
+    console.log(inputEl.value);}, DEBOUNCE_DELAY));
+
+   
+   
+//     const result = _.add(2, 3);
+// console.log(result);
+
+
+// document.addEventListener(
+//     "scroll",
+//     _.debounce(() => {
+//       console.log("Scroll handler call after 300ms pause");
+//     }, 300)
+//   );
